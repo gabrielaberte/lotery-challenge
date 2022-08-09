090eb7aea7f7bdc3f975ee7016082f0a9a13d081
Gabe Berté <88408646+gabrielaberte@users.noreply.github.com>
2022-08-09 13:24:45 -0300

    :sparkles: feat: criando contexto

---
+++ lotery/src/components/context/context.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from "react";
+
+export const Context = React.createContext<any>({});
+
+export function ContextProvider(props: {
+  children:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}): any {
+  const [nomeSorteio, setNomeSorteio] = useState<string>("megasena");
+  const [concurso, setConcurso] = useState<number | undefined>();
+
+  return (
+    <Context.Provider
+      value={{
+        nomeSorteio,
+        setNomeSorteio,
+        concurso,
+        setConcurso,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+}
+
+export function useContexto(): any {
+  const context = useContext(Context);
+
+  if (!context) {
+    throw new Error("Sem contexto");
+  }
+
+  return context;
+}
